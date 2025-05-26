737d2a1f18cc1c592d6a53cf193f9211cf192b76
juliya.lysenko <juliya.deleh@gmail.com>
2025-05-26 21:24:17 +0300

    commit-snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,50 @@
-console.log('sn');
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+const refs = {
+  form: document.querySelector('.form'),
+  fieldset: document.querySelector('fieldset'),
+  inputDelay: document.querySelector('input[type="number"]'),
+  fulfilled: document.querySelector('input[value="fulfilled"]'),
+  rejected: document.querySelector('input[value="rejected"]'),
+  button: document.querySelector('button[type="submit"]'),
+};
+
+function onButtonSubmit(event) {
+  event.preventDefault();
+  const delay = parseInt(refs.inputDelay.value);
+  const inputState = document.querySelector('input[name="state"]:checked');
+  const state = inputState.value;
+
+  const promise = new Promise((resolve, reject) => {
+    if (state === 'fulfilled') {
+      setTimeout(() => {
+        resolve(delay);
+      }, delay);
+    } else if (state === 'rejected') {
+      setTimeout(() => {
+        reject(delay);
+      }, delay);
+    }
+  });
+
+  promise
+    .then(result => {
+      iziToast.success({
+        title: 'Notification',
+        message: `✅ Fulfilled promise in ${result}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(error => {
+      iziToast.error({
+        title: 'Notification',
+        message: `❌ Rejected promise in ${error}ms`,
+        position: 'topRight',
+      });
+    });
+}
+
+refs.form.addEventListener('submit', onButtonSubmit);
